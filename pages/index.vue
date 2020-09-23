c30b4238fcce82529655aa3a7f812838828750a2
Goncalo Proenca <gjproenca@gmail.com>
2020-09-23 17:40:41 +0100

    improve index layout

--- pages/index.vue
+++ pages/index.vue
@@ -1,62 +1,72 @@
 <template>
-  <div class="card mt-3">
-    <div class="card-body">
-      <div class="card-title">
-        <h3>Chat Group</h3>
-        <hr />
-      </div>
-      <div class="card-body">
-        <div class="messages" v-for="(msg, index) in messages" :key="index">
-          <p>
-            <span class="font-weight-bold">{{ msg.user }}: </span
-            >{{ msg.message }}
-          </p>
-        </div>
-      </div>
-    </div>
-    <div class="card-footer">
-      <form @submit.prevent="sendMessage">
-        <div class="gorm-group">
-          <label for="user">User:</label>
-          <input type="text" v-model="user" class="form-control" />
-        </div>
-        <div class="gorm-group pb-3">
-          <label for="message">Message:</label>
-          <input type="text" v-model="message" class="form-control" />
-        </div>
-        <button type="submit" class="btn btn-success">Send</button>
-      </form>
-    </div>
+  <div>
+    <v-container style="height: 100vh" fill-height fluid>
+      <v-row class="d-flex justify-center">
+        <v-card>
+          <v-form>
+            <v-row class="pl-7 pr-7">
+              <v-col cols="12">
+                <v-text-field
+                  v-model="email"
+                  :rules="emailRules"
+                  name="email"
+                  label="E-mail"
+                ></v-text-field>
+              </v-col>
+
+              <v-col cols="12">
+                <v-text-field
+                  v-model="password"
+                  :append-icon="showPassword ? 'mdi-eye' : 'mdi-eye-off'"
+                  :rules="[passwordRules.required, passwordRules.min]"
+                  :type="showPassword ? 'text' : 'password'"
+                  name="password"
+                  label="Password"
+                  hint="At least 8 characters"
+                  counter
+                  @click:append="showPassword = !showPassword"
+                ></v-text-field>
+              </v-col>
+            </v-row>
+
+            <v-row>
+              <v-col class="d-flex justify-center" cols="12">
+                <v-btn @click="login"> Login </v-btn>
+              </v-col>
+              <v-col class="d-flex justify-center" cols="12">
+                <p>Don't have an account? Register</p>
+              </v-col>
+            </v-row>
+          </v-form>
+        </v-card>
+      </v-row>
+    </v-container>
   </div>
 </template>
 
 <script>
-import io from 'socket.io-client'
 export default {
   data() {
     return {
-      user: '',
-      message: '',
-      messages: [],
-      socket: io('localhost:3001'),
+      email: '',
+      emailRules: [
+        (v) => !!v || 'E-mail is required',
+        (v) => /.+@.+\..+/.test(v) || 'E-mail must be valid',
+      ],
+      password: '',
+      showPassword: false,
+      passwordRules: {
+        required: (value) => !!value || 'Password is equired',
+        min: (v) => v.length >= 8 || 'Min 8 characters',
+      },
     }
   },
-  mounted() {
-    this.socket.on('MESSAGE', (data) => {
-      this.messages = [...this.messages, data]
-      // you can also do this.messages.push(data)
-    })
-  },
   methods: {
-    sendMessage(e) {
-      e.preventDefault()
-
-      this.socket.emit('SEND_MESSAGE', {
-        user: this.user,
-        message: this.message,
-      })
-      this.message = ''
+    login() {
+      // TODO: Send login details or register user
     },
   },
 }
 </script>
+
+<style lang="scss" scoped></style>
