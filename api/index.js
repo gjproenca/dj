42d870cf3c346e3e6b3acbc5e231b44a0d4c72fe
gjproenca <gjproenca@gmail.com>
2020-11-17 17:01:16 +0000

    fix(playlist): fix 413 when creating/posting playlist to db

--- api/index.js
+++ api/index.js
@@ -10,7 +10,7 @@ const playlists = require('./routes/playlists.js')
 const app = express()
 
 // Init body-parser options (inbuilt with express)
-app.use(express.json())
+app.use(express.json({ limit: '50mb', type: 'application/json' }))
 app.use(express.urlencoded({ extended: true }))
 
 // Use API Routes
